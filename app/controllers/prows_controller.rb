947de40a12b354b8415c7919c5150d94088fe169
Maryam_Ebrahimi <maryam.ebrahimi25@yahoo.com>
2015-05-09 12:17:29 +0430

    alaki7

--- app/controllers/prows_controller.rb
+++ app/controllers/prows_controller.rb
@@ -71,9 +71,9 @@ class ProwsController < ApplicationController
   # PATCH/PUT /prows/1
   # PATCH/PUT /prows/1.json
   def update
+    @interval = 4.hours + 30.minutes
+    @prow.start_time = @prow.start_time - @interval
     respond_to do |format|
-      @interval = 4.hours + 30.minutes
-      @prow.start_time = @prow.start_time - @interval
       if @prow.update(prow_params)
         Drug.set_drug(@prow.drug.id,params[:drug_name],params[:drug_description])
         @box = BoxPart.find_by_part_num(params[:box]);  # check it is empty !important
